c3aa976c1f9d9facc7aee63636c726dc6f95c53a
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2016-11-05 18:42:35 +0200

    browser fix

--- ttk91js.js
+++ ttk91js.js
@@ -1,9 +1,18 @@
 var compile = require('./ttk91js.compile.js');
 var Machine = require('./ttk91js.machine.js');
 
-module.exports = {
-	compile: compile,
-	createMachine: function(settings) {
-		return new Machine(settings);
+(function(root) {
+	var module = {
+		compile: compile,
+		createMachine: function(settings) {
+			return new Machine(settings);
+		}
+	};
+
+	if(typeof module !== 'undefined' && 'exports' in module) {
+		module.exports = module;
+	} else {
+		root.ttk91js = module;
 	}
-};
+})(this);
+
