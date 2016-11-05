8980995a40f2e2665316edc8eb784a4af8048ff2
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2016-11-05 23:47:52 +0200

    all good everything

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -4,6 +4,11 @@ npm test
 
 RESULT=$?
 
+if [ $RESULT -eq 0 ]; then
+	gulp build-dist
+	git add dist/*
+fi
+
 git stash pop -q
 
 [ $RESULT -ne 0 ] && exit 1
--- hooks/pre-push.sh
+++
@@ -1,6 +0,0 @@
-gulp build-dist
-
-git add dist/*
-git commit --no-verify -m 'automatically generated distribution'
-
-exit 0
