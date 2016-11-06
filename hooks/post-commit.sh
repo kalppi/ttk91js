1112c4fe1f3f60f1e46d9e3b83b4c2bb60ecda4b
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2016-11-06 19:29:20 +0200

    fixes

--- hooks/post-commit.sh
+++ hooks/post-commit.sh
@@ -1,4 +1,4 @@
-if git diff --name-only --cached | grep 'ttk91js.*js'; then
+if git show --name-only --pretty= | grep 'ttk91js.*\.js'; then
 	gulp build-dist
 	git add dist/*
 	git commit --no-verify -m "automatically generated distributions"
