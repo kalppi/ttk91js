06785eb06d7125a00e94528d7be25a75673be1f6
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2016-11-06 22:19:04 +0200

    do not apply to dist files

--- hooks/post-commit.sh
+++ hooks/post-commit.sh
@@ -1,13 +1,5 @@
-echo 111
-
-if git show --name-only --pretty= | grep 'ttk91js.*\.js'; then
-	echo 222
+if git show --name-only --pretty= | grep 'ttk91js.*\.js' | grep -v 'dist/'; then
 	gulp build-dist
-	echo 333
 	git add dist/*
-	echo 444
 	git commit --no-verify -m "automatically generated distributions"
-	echo 555
 fi
-
-echo 666
