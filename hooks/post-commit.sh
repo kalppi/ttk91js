c2d2386e68d4316de1f182c8f72ccea9da060c8e
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2016-11-06 22:11:35 +0200

    test

--- hooks/post-commit.sh
+++ hooks/post-commit.sh
@@ -1,5 +1,13 @@
+echo 111
+
 if git show --name-only --pretty= | grep 'ttk91js.*\.js'; then
+	echo 222
 	gulp build-dist
+	echo 333
 	git add dist/*
+	echo 444
 	git commit --no-verify -m "automatically generated distributions"
+	echo 555
 fi
+
+echo 666
