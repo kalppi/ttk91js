76c5029f5f70f3194eabcdadcf1135bf8f46ff32
Jarno Luukkonen <luukkonen.jarno@gmail.com>
2016-11-07 14:47:07 +0200

    automatically generated distributions

--- dist/ttk91js.dist.js
+++ dist/ttk91js.dist.js
@@ -865,24 +865,77 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     function prepare(code) {
       var lines = code.split('\n');
       var instructions = [];
-      var lineMap = {};
+      var sourceMap = {};
 
       var symbols = [];
       var data = [];
 
-      for (var l = 0; l < lines.length; l++) {
-        var line = lines[l].trim();
+      function isSymbol(s) {
+        if (s[0] == '=' || s[0] == '@') {
+          s = s.substring(1);
+        }
 
-        var i = line.indexOf(';');
+        return (/^[a-z]+$/i.test(s)
+        );
+      }
+
+      function getSymbol(s) {
+        if (s[0] == '=' || s[0] == '@') {
+          s = s.substring(1);
+        }
+
+        return s;
+      }
+
+      function symbolExists(s) {
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+          for (var _iterator = symbols[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var sym = _step.value;
+
+            if (sym.name == s) return true;
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
+          }
+        }
+
+        switch (s) {
+          case 'CRT':
+          case 'HALT':
+            return true;
+        }
+
+        return false;
+      }
+
+      var _loop = function _loop(l) {
+        line = lines[l].trim();
+        i = line.indexOf(';');
+
         if (i != -1) {
           line = line.substring(0, i);
         }
 
         if (line.length === 0) {
-          continue;
+          return "continue";
         }
 
-        var parts = line.split(/[\s]+/);
+        parts = line.split(/[\s]+/);
+
 
         if (OPS.indexOf(parts[0]) == -1) {
           symbols.push({
@@ -917,7 +970,8 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
           if (parts.length == 3) {
             i = parts[2].indexOf('(');
             if (i != -1) {
-              var j = parts[2].indexOf(')', i);
+              j = parts[2].indexOf(')', i);
+
               if (j == -1) {
                 throw new Ttk91jsCompileException('syntax error', l);
               } else {
@@ -945,7 +999,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
             }
           }
 
-          if (parts[0] != 'DC' && OPS.indexOf(parts[0]) == -1) {
+          if (OPS.indexOf(parts[0]) == -1) {
             throw new Ttk91jsCompileException('unknown opcode (' + parts[0] + ')', l);
           }
 
@@ -957,9 +1011,60 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
             }
           });
 
-          lineMap[instructions.length] = l;
+          sourceMap[instructions.length] = l;
 
-          instructions.push(parts);
+          instructions.push({
+            line: l,
+            code: parts
+          });
+        }
+      };
+
+      for (var l = 0; l < lines.length; l++) {
+        var line;
+        var i;
+        var parts;
+        var j;
+
+        var _ret = _loop(l);
+
+        if (_ret === "continue") continue;
+      }
+
+      var _iteratorNormalCompletion2 = true;
+      var _didIteratorError2 = false;
+      var _iteratorError2 = undefined;
+
+      try {
+        for (var _iterator2 = instructions[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+          var ins = _step2.value;
+
+          if (ins.code.length == 2 && isSymbol(ins.code[1])) {
+            var symbol = getSymbol(ins.code[1]);
+
+            if (!symbolExists(symbol)) {
+              throw new Ttk91jsCompileException('unknown symbol (' + symbol + ')', ins.line);
+            }
+          } else if (ins.code.length == 4 && isSymbol(ins.code[2])) {
+            var _symbol = getSymbol(ins.code[2]);
+
+            if (!symbolExists(_symbol)) {
+              throw new Ttk91jsCompileException('unknown symbol (' + _symbol + ')', ins.line);
+            }
+          }
+        }
+      } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion2 && _iterator2.return) {
+            _iterator2.return();
+          }
+        } finally {
+          if (_didIteratorError2) {
+            throw _iteratorError2;
+          }
         }
       }
 
@@ -967,7 +1072,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         code: instructions,
         symbols: symbols,
         data: data,
-        lineMap: lineMap
+        sourceMap: sourceMap
       };
     }
 
@@ -980,8 +1085,6 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
             return data.symbols[i].addr;
           }
         }
-
-        throw new Ttk91jsCompileException('unknown symbol (' + symbol + ')');
       }
 
       function isRegister(reg) {
@@ -1002,14 +1105,15 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         delete data.symbols[i].type;
       }
 
-      var _iteratorNormalCompletion = true;
-      var _didIteratorError = false;
-      var _iteratorError = undefined;
+      var _iteratorNormalCompletion3 = true;
+      var _didIteratorError3 = false;
+      var _iteratorError3 = undefined;
 
       try {
-        for (var _iterator = data.code[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-          var d = _step.value;
+        for (var _iterator3 = data.code[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+          var ins = _step3.value;
 
+          var d = ins.code;
           var op = global.OP[d[0]];
 
           var rj = 0;
@@ -1069,21 +1173,21 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
           words.push(word);
         }
       } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
+        _didIteratorError3 = true;
+        _iteratorError3 = err;
       } finally {
         try {
-          if (!_iteratorNormalCompletion && _iterator.return) {
-            _iterator.return();
+          if (!_iteratorNormalCompletion3 && _iterator3.return) {
+            _iterator3.return();
           }
         } finally {
-          if (_didIteratorError) {
-            throw _iteratorError;
+          if (_didIteratorError3) {
+            throw _iteratorError3;
           }
         }
       }
 
-      return { lines: data.code, code: words, symbols: data.symbols, data: data.data, lineMap: data.lineMap };
+      return { code: words, symbols: data.symbols, data: data.data, sourceMap: data.sourceMap };
     };
 
     module.exports = compile;
@@ -1245,16 +1349,6 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
       function Ttk91jsRuntimeException(message, line) {
         this.name = 'Ttk91jsRuntimeException';
-        this.message = message;
-        this.line = line;
-      }
-
-      Ttk91jsRuntimeException.prototype.toString = function () {
-        return this.name + ': ' + this.message;
-      };
-
-      function Ttk91jsApiException(message, line) {
-        this.name = 'Ttk91jsApiException';
         this.message = message;
         this.line = line;
       }
